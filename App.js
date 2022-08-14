b7023fbc62ebc014719d296fcbd9b30a496925c9
abi-widiarta <abiwidiarta03@gmail.com>
2022-08-14 14:54:30 +0700

    Buat pages CallAPIGet berhasil get data dari dummy API jsonplaceholder ssama reqres berhasil post data dengan struktur liat di jsonplaceholder dan api pakai di reqres

--- App.js
+++ App.js
@@ -8,55 +8,19 @@ import {
   ScrollView,
 } from 'react-native';
 import Cart from './src/components/Cart';
-import PropsStory from './src/pages/PropsStory/index';
 import StateCounter from './src/pages/StateCounter';
 import KomunikasiAntarKomponen from './src/pages/KomunikasiAntarKomponen';
+import CallAPIGet from './src/pages/CallAPIGet';
 
 const App = () => {
   return (
-    // <ScrollView horizontal>
-    //   <View style={styles.wrapper}>
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //     <PropsStory
-    //       name="tes"
-    //       url="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-15/296912449_339476318275959_578721334857320555_n.jpg?stp=c0.499.1284.1284a_dst-jpg_e35_s150x150&_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=-UjcRcGaet0AX_N3Bp0&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AT_8LsNdEgu83X0goBLGzUUCbnX78ov8psRREtV2wQTNlQ&oe=62F9C433&_nc_sid=acd11b"
-    //     />
-    //   </View>
-    // </ScrollView>
-
     // <StateCounter />
     // <KomunikasiAntarKomponen />
+
+    <View>
+      <Text>Tes Get API</Text>
+      <CallAPIGet />
+    </View>
   );
 };
 
