3f80009d5194fe115dfe0f792855d9fc2368fca4
abi-widiarta <abiwidiarta03@gmail.com>
2022-08-17 21:30:59 +0700

    Berhasil buat modal tidak avoid keyboad saat tekan textInput

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,11 @@
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import React, {useState} from 'react';
 import Modal from 'react-native-modal';
 import PlusButton from './plusButton';
@@ -14,21 +21,70 @@ function ModalComponent() {
     <View>
       <PlusButton onPress={toggleModal} />
       <Modal
+        avoidKeyboard={true}
         onBackButtonPress={toggleModal}
         backdropTransitionOutTiming={0}
         swipeDirection={'down'}
         isVisible={isModalVisible}
-        style={{margin: 0}}>
-        <View style={{marginTop: 'auto'}}>
-          <View
-            style={{
-              height: 400,
-              backgroundColor: 'white',
-              borderTopLeftRadius: 15,
-              borderTopRightRadius: 15,
-            }}></View>
-          <Button title="Hide modal" onPress={toggleModal} />
-        </View>
+        style={{margin: 0, position: 'relative'}}>
+        <ScrollView
+          style={{
+            borderWidth: 2,
+            bottom: 0,
+            marginTop: '30%',
+            height: 600,
+            backgroundColor: 'white',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            paddingVertical: 40,
+          }}>
+          {/* name activity */}
+          <View style={{marginVertical: 20}}>
+            <Text
+              style={{fontSize: 13, color: '#9F9F9F', paddingHorizontal: 30}}>
+              Name your activity
+            </Text>
+            <TextInput
+              style={{
+                paddingHorizontal: 30,
+                borderBottomWidth: 1,
+                borderColor: 'rgba(0,0,0,0.1)',
+                color: '#252525',
+                fontSize: 18,
+              }}></TextInput>
+          </View>
+          {/* what is it about */}
+          <View style={{marginVertical: 20}}>
+            <Text
+              style={{fontSize: 13, color: '#9F9F9F', paddingHorizontal: 30}}>
+              What is is about?
+            </Text>
+            <TextInput
+              style={{
+                paddingHorizontal: 30,
+                borderBottomWidth: 1,
+                borderColor: 'rgba(0,0,0,0.1)',
+                color: '#252525',
+                fontSize: 13,
+              }}></TextInput>
+          </View>
+          {/* pick icon */}
+          <View style={{marginVertical: 20}}>
+            <Text
+              style={{fontSize: 13, color: '#9F9F9F', paddingHorizontal: 30}}>
+              Pick an icon for your activity
+            </Text>
+            <TextInput
+              style={{
+                paddingHorizontal: 30,
+                borderBottomWidth: 1,
+                borderColor: 'rgba(0,0,0,0.1)',
+                color: '#252525',
+                fontSize: 13,
+              }}></TextInput>
+          </View>
+        </ScrollView>
+        <Button title="Hide modal" onPress={toggleModal} />
       </Modal>
     </View>
   );
